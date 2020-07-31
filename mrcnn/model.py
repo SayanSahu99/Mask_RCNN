3771e380f02eb0e69a2a6df133ef3d57a8fe0b61
SayanSahu99 <sayansovan@gmail.com>
2020-08-01 00:26:21 +0530

    added a new layer

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1006,6 +1006,15 @@ def build_fpn_mask_graph(rois, feature_maps, image_meta,
                            name='mrcnn_mask_bn4')(x, training=train_bn)
     x = KL.Activation('relu')(x)
 
+    # added
+    x = KL.TimeDistributed(KL.Conv2D(256, (3, 3), padding="same"),
+                           name="mrcnn_mask_conv5")(x)
+    # added
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_mask_bn5')(x, training=train_bn)
+    # added
+    x = KL.Activation('relu')(x)
+
     x = KL.TimeDistributed(KL.Conv2DTranspose(256, (2, 2), strides=2, activation="relu"),
                            name="mrcnn_mask_deconv")(x)
     x = KL.TimeDistributed(KL.Conv2D(num_classes, (1, 1), strides=1, activation="sigmoid"),
