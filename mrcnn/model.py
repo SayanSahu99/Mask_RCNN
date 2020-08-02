eca0bae89204ac6e3ae8944f597cb4374bebd518
SayanSahu99 <sayansovan@gmail.com>
2020-08-02 16:22:17 +0530

    added laplace

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1228,131 +1228,22 @@ def mrcnn_mask_edge_loss_graph(y_pred, y_true, edge_filters, smoothing_predictio
             loss instead of using it as a L^p norm
     :return: loss
     """
-    # sobel kernels
-    # sobel_x_kernel = tf.reshape(tf.constant([[1, 2, 1],
-    #                                          [0, 0, 0],
-    #                                          [-1, -2, -1]], dtype=tf.float32),
-    #                             shape=[3, 3, 1, 1], name='sobel_x_kernel')
-    # sobel_y_kernel = tf.reshape(tf.constant([[1, 0, -1],
-    #                                          [2, 0, -2],
-    #                                          [1, 0, -1]], dtype=tf.float32),
-    #                             shape=[3, 3, 1, 1], name='sobel_y_kernel')
-
-    # prewitt kernels
-    prewitt_x_kernel = tf.reshape(tf.constant([[1, 0, -1],
-                                             [1, 0, -1],
-                                             [1, 0, -1]], dtype=tf.float32),
-                                shape=[3, 3, 1, 1], name='prewitt_x_kernel')
-    prewitt_y_kernel = tf.reshape(tf.constant([[1, 1, 1],
-                                             [0, 0, 0],
-                                             [-1, -1, -1]], dtype=tf.float32),
-                                shape=[3, 3, 1, 1], name='prewitt_y_kernel')
-
-    # # prewitt kernels
-    # kayyali_senw_kernel = tf.reshape(tf.constant([[6, 0, -6],
-    #                                          [0, 0, -0],
-    #                                          [-6, 0, 6]], dtype=tf.float32),
-    #                             shape=[3, 3, 1, 1], name='kayyali_senw_kernel')
-    # kayyali_nesw_kernel = tf.reshape(tf.constant([[-6, 0, 6],
-    #                                          [0, 0, 0],
-    #                                          [6, 0, -6]], dtype=tf.float32),
-    #                             shape=[3, 3, 1, 1], name='kayyali_nesw_kernel')
-
-    # # roberts kernels
-    # roberts_x_kernel = tf.reshape(tf.constant([[1, 0],
-    #                                           [0, -1]], dtype=tf.float32),
-    #                             shape=[2, 2, 1, 1], name='roberts_x_kernel')
-    # roberts_y_kernel = tf.reshape(tf.constant([[0, -1],
-    #                                            [1, 0]], dtype=tf.float32),
-    #                             shape=[2, 2, 1, 1], name='roberts_y_kernel')
-
-    # laplace kernel
-    # laplacian_kernel = tf.reshape(tf.constant([[1, 1, 1],
-    #                                            [1, -8, 1],
-    #                                            [1, 1, 1]], dtype=tf.float32),
-    #                               shape=[3, 3, 1, 1], name='laplacian_kernel')
-
+   
     gaussian_kernel = tf.reshape(tf.constant([[0.077847, 0.123317, 0.077847],
                                               [0.123317, 0.195346, 0.1233179],
                                               [0.077847, 0.123317, 0.077847]], dtype=tf.float32),
                                  shape=[3, 3, 1, 1], name='gaussian_kernel')
-    
-    import numpy as np
-    from PIL import Image
-    import matplotlib.pyplot as plt
+     # laplace kernel
+    laplacian_kernel = tf.reshape(tf.constant([[1, 1, 1],
+                                               [1, -8, 1],
+                                               [1, 1, 1]], dtype=tf.float32),
+                                  shape=[3, 3, 1, 1], name='laplacian_kernel')
 
-    # Open the image
-    img = np.array(Image.open('dancing-spider.jpg')).astype(np.uint8)
-
-    # Apply gray scale
-    gray_img = np.round(0.299 * img[:, :, 0] +
-                        0.587 * img[:, :, 1] +
-                        0.114 * img[:, :, 2]).astype(np.uint8)
-
-    # Prewitt Operator
-    h, w = gray_img.shape
-    # define filters
-    horizontal = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])  # s2
-    vertical = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])  # s1
-
-    # define images with 0s
-    newgradientImage = np.zeros((h, w))
-
-    # offset by 1
-    for i in range(1, h - 1):
-        for j in range(1, w - 1):
-            horizontalGrad = (horizontal[0, 0] * gray_img[i - 1, j - 1]) + \
-                            (horizontal[0, 1] * gray_img[i - 1, j]) + \
-                            (horizontal[0, 2] * gray_img[i - 1, j + 1]) + \
-                            (horizontal[1, 0] * gray_img[i, j - 1]) + \
-                            (horizontal[1, 1] * gray_img[i, j]) + \
-                            (horizontal[1, 2] * gray_img[i, j + 1]) + \
-                            (horizontal[2, 0] * gray_img[i + 1, j - 1]) + \
-                            (horizontal[2, 1] * gray_img[i + 1, j]) + \
-                            (horizontal[2, 2] * gray_img[i + 1, j + 1])
-
-            verticalGrad = (vertical[0, 0] * gray_img[i - 1, j - 1]) + \
-                        (vertical[0, 1] * gray_img[i - 1, j]) + \
-                        (vertical[0, 2] * gray_img[i - 1, j + 1]) + \
-                        (vertical[1, 0] * gray_img[i, j - 1]) + \
-                        (vertical[1, 1] * gray_img[i, j]) + \
-                        (vertical[1, 2] * gray_img[i, j + 1]) + \
-                        (vertical[2, 0] * gray_img[i + 1, j - 1]) + \
-                        (vertical[2, 1] * gray_img[i + 1, j]) + \
-                        (vertical[2, 2] * gray_img[i + 1, j + 1])
-
-            # Edge Magnitude
-            mag = np.sqrt(pow(horizontalGrad, 2.0) + pow(verticalGrad, 2.0))
-            newgradientImage[i - 1, j - 1] = mag
-
-
-    plt.figure()
-    plt.title('dancing-spider-prewitt.png')
-    plt.imsave('dancing-spider-prewitt.png', newgradientImage, cmap='gray', format='png')
-    plt.imshow(newgradientImage, cmap='gray')
-    plt.show()
-    print(wanlee)
-
-    # filter_map = {
-    #     "sobel-x": sobel_x_kernel,
-    #     "sobel-y": sobel_y_kernel,
-    #     "roberts-x": roberts_x_kernel,
-    #     "roberts-y": roberts_y_kernel,
-    #     "prewitt-x": prewitt_x_kernel,
-    #     "prewitt-y": prewitt_y_kernel,
-    #     "kayyali-senw": kayyali_senw_kernel,
-    #     "kayyali-nesw": kayyali_nesw_kernel,        
-    #     "laplace": laplacian_kernel
-    # }
-
-    filter_map = {
-        
-        "prewitt-x": prewitt_x_kernel,
-        "prewitt-y": prewitt_y_kernel,
-        
+    filter_map = {   
+        "laplace": laplacian_kernel
     }
 
-
+   
     lp_norm_map = {
         "l1": 1,
         "l2": 2,
